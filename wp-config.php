b7ce27562ffff08f62676283741b5e1d8b72c75e
Stefanos Missin <s.missin@zonepage.gr>
2021-11-01 10:33:44 +0200

    rustoleum first commit

---
+++ wp-config.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Οι βασικές ρυθμίσεις για to WordPress
+ *
+ * Το wp-config.php χρησιμοποιείται από την δέσμη ενεργειών κατά την
+ * διαδικασία εγκατάστασης. Δεν χρειάζεται να χρησιμοποιήσετε τον ιστότοπο, μπορείτε
+ * να αντιγράψετε αυτό το αρχείο ως "wp-config.php" και να συμπληρώσετε τις παραμέτρους.
+ *
+ * Αυτό το αρχείο περιέχει τις ακόλουθες ρυθμίσεις:
+ *
+ * * MySQL ρυθμίσεις
+ * * Κλειδιά ασφαλείας
+ * * Πρόθεμα πινάκων βάσης δεδομένων
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL ρυθμίσεις - Μπορείτε να λάβετε αυτές τις πληροφορίες από τον φιλοξενητή σας ** //
+/** Το όνομα της βάσης δεδομένων του WordPress */
+define( 'DB_NAME', 'wp_ads0980aj12h3' );
+
+/** Ψευδώνυμο χρήσης MySQL */
+define( 'DB_USER', 'root' );
+
+/** Συνθηματικό βάσης δεδομένων MySQL */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Charset της βάσηςη δεδομένων που θα χρησιμοποιηθεί στην δημιουργία των πινάκων. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Τύπος Collate της βάσης δεδομένων. Μην το αλλάζετε αν έχετε αμφιβολίες. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Μοναδικά κλειδιά πιστοποίησηςη και Salts.
+ *
+ * Αλλάξτε τα σε διαφορετικά μοναδικές φράσεις!
+ * Μπορείτε να δημιουργήσετε χρησιμοποιώντας {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * Μπορείτε να τα αλλάξετε οποτεδήποτε για να ακυρώσετε τα υπάρχοντα cookies. Θα υποχρεώσει όλους χρήστες να επανασυνδεθούν.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'poksdonWtfSWU+eh9O}k{_BKsVJaE?t9p4w2K5Z@hrb[CzMDH2Au p8r!dQEX<qF' );
+define( 'SECURE_AUTH_KEY',  ']g`;Kh>}K(q-!#.7Ay#vkX%lI?Pgz#6s)MHUL%6XKEg.;W;^s/)e**ujG&D5]olr' );
+define( 'LOGGED_IN_KEY',    '|QsNXv@O{;|EM|nY&P#*O8_tfw/29knV{wLe~IiKFdR?Q:q>4l~}]:M.|U+Nh)wx' );
+define( 'NONCE_KEY',        '8(RhEey+ 98|~9+p(f|~BYi:--C$-N^JM@C#H~PrMWmcYG#T/1[amNxdg(U6r[4Y' );
+define( 'AUTH_SALT',        'nj@O`zqEB[Ep;,I{:rwAIC([%9z+.Md/S> o1#F>wv*0b0Wn^peXs SLW:p*C%6]' );
+define( 'SECURE_AUTH_SALT', 'Z3BXn>)^6L@l7%Mba*$&t`L%e0Sm~gzI5U$}u*z7w{M$r4!G-H=Ta(5m4~nLIu@+' );
+define( 'LOGGED_IN_SALT',   '<dR!`=`84>Hp[dmgIO$`yPdrLY+305Se]I,]u,DOHmYkVPbQ)Al@Yqh1EQkIle45' );
+define( 'NONCE_SALT',       '{mD!e|:m~G,JbrfU]R#.0=L>oTO8F/w*]KT[DrzT7%Z%28qj3Ij6FbX7TnRB:,^7' );
+
+/**#@-*/
+
+/**
+ * Πρόθεμα Πίνακα Βάσης Δεδομένων του WordPress.
+ *
+ * Μπορείτε να έχετε πολλαπλές εγκαταστάσεις σε μια βάση δεδομένων αν δώσετε σε κάθε μία
+ * ένα μοναδικό πρόθεμα. Μόνο αριθμοί, γράμματα και κάτω παύλα παρακαλούμε!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Για προγραμματιστές: Κατάσταση Απασφαλμάτωσης WordPress (Debugging Mode).
+ *
+ * Αλλάξτε το σε true για να ενεργοποιήσετε την εμφάνισης ειδοποιήσεων για την διαδικασία ανάπτυξης.
+ * Η χρήση WP_DEBUG προτείνεται για τους δημιουργούς προσθέτων και θεμάτων
+ * στο περιβάλλον ανάπτυξης τους.
+ *
+ * Για πληροφορίες για άλλες σταθερές που μπορούν να χρησιμοποιηθούν για απασφαλμάτωση,
+ * επισκευθείτε το Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* Αυτό είναι όλο, σταματήστε γράφετε! Χαρούμενο blogging. */
+
+/** Η απόλυτη διαδρομή τον κατάλογο του WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Ορίζει τις μεταβλητές και τα περιλαμβανόμενα αρχεία WordPress. */
+require_once(ABSPATH . 'wp-settings.php');
