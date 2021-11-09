ea255b237bc888563264922442d82a898406a5eb
Stefanos Missin <s.missin@zonepage.gr>
2021-11-09 11:57:24 +0200

    Add Home General Template - custom css/js files

---
+++ wp-content/themes/rustoleum/rustoleum/home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Template Name: HOME GENERAL
+*
+*/
+
+get_header();
+?>
+
+
+
+<main id="primary" class="site-main">
+    <div class="container-fluid">
+        <div class="row ">
+        </div>
+    </div>
+</main>
+
+<?php
+get_footer();
