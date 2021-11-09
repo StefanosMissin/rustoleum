a13cf854e91ffb6af91c245f2a3982b84d7dfbf0
Stefanos Missin <s.missin@zonepage.gr>
2021-11-09 12:19:38 +0200

    Create Home General

--- wp-content/themes/rustoleum/rustoleum/header.php
+++ wp-content/themes/rustoleum/rustoleum/header.php
@@ -17,6 +17,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"  />
+	<link rel="stylesheet" href="/wp-content/themes/tikkurila/custom.css">
 	<?php wp_head(); ?>
 </head>
 
--- wp-content/themes/rustoleum/rustoleum/home.php
+++ wp-content/themes/rustoleum/rustoleum/home.php
@@ -5,15 +5,44 @@
 */
 
 get_header();
+
+$left_image = get_field('home_general_image_left');
+$right_image = get_field('home_general_image_right');
+$title = get_field('home_general_title');
+$text = get_field('home_general_text');
+$button_text = get_field('home_general_button_text');
+$button_link = get_field('home_general_button_link');
+
 ?>
 
 
 
 <main id="primary" class="site-main">
-    <div class="container-fluid">
-        <div class="row ">
+    <section id="home_general_hero">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 left_image">
+                    <img src="<?php echo $left_image; ?>">
+                    <p class="image-text"><?php _e("Διακόσμηση","zonepage"); ?></p>
+                </div>
+                <div class="col-md-6 right_image">
+                    <img src="<?php echo $right_image; ?>">
+                    <p class="image-text"><?php _e("Συντήρηση & Προστασία","zonepage"); ?></p>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
+
+    <section id="home_general_texts">
+        <div class="container">
+            <div class="col-md-12">
+                <h1><?php echo $title; ?></h1>
+                <div class="text">
+                    <?php echo $text; ?>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
